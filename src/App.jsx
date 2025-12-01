5d27664b0c6449ab9be5d537b05262514f43562b
Frida Henriksen <fridahenriksen@hotmail.com>
2025-12-01 15:42:34 +0100

    css style reset

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <nav className="navbar">Navbar content</nav>
+      <main>Main Content</main>
+    </div>
+  );
+};
+
+export default App;
