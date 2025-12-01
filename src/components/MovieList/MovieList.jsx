9557d7c59ad3042ee16e3783d4bee82c2a9c945e
Frida Henriksen <fridahenriksen@hotmail.com>
2025-12-01 18:06:07 +0100

    MoviList component to app.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -8,10 +8,19 @@ const MovieList = () => {
         <h2 className="movie_list_heading">Popular</h2>
         <div className="movie_list_fs">
           <ul className="movie_filter">
-            <li className="movie_filter_item"></li>
-            <li className="movie_filter_item"></li>
-            <li className="movie_filter_item"></li>
+            <li className="movie_filter_item">8+ Star</li>
+            <li className="movie_filter_item">7+ Star</li>
+            <li className="movie_filter_item">6+ Star</li>
           </ul>
+          <select name="" id="" className="movie_sorting">
+            <option value="">SortBy</option>
+            <option value="">Date</option>
+            <option value="">Rating</option>
+          </select>
+          <select name="" id="" className="movie_sorting">
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
+          </select>
         </div>
       </header>
     </section>
