5138b4f9cecb866cd9c528801b53d8587dcf6984
Frida Henriksen <fridahenriksen@hotmail.com>
2026-01-23 17:01:54 +0100

    filter method

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,6 +4,7 @@ import MovieCard from "./MovieCard";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [minRating, setMinRating] = useState(0);
   useEffect(() => {
     fetchMovies();
   }, []);
@@ -15,15 +16,31 @@ const MovieList = () => {
     const data = await response.json();
     setMovies(data.results);
   };
+
+  const handleFilter = (rate) => {
+    setMinRating(rate);
+
+    const filtered = movies.filter((movie) => movie.vote_average >= rate);
+    setMovies(filtered);
+  };
   return (
     <section className="movie_list">
       <header className="align_center movie_list_header">
         <h2 className=" align_center movie_list_heading">Popular</h2>
         <div className="align_center movie_list_fs">
           <ul className="align_center movie_filter">
-            <li className="movie_filter_item active">8+ Star</li>
-            <li className="movie_filter_item">7+ Star</li>
-            <li className="movie_filter_item">6+ Star</li>
+            <li
+              className="movie_filter_item active"
+              onClick={() => handleFilter(8)}
+            >
+              8+ Star
+            </li>
+            <li className="movie_filter_item" onClick={() => handleFilter(7)}>
+              7+ Star
+            </li>
+            <li className="movie_filter_item" onClick={() => handleFilter(6)}>
+              6+ Star
+            </li>
           </ul>
           <select name="" id="" className="movie_sorting">
             <option value="">SortBy</option>
