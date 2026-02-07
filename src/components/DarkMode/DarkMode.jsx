9a1adf69eec761492b22db6d658234157c26e844
Frida Henriksen <fridahenriksen@hotmail.com>
2026-02-07 17:59:46 +0100

    dark mode default

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -11,9 +11,23 @@ const DarkMode = () => {
     document.querySelector("body").setAttribute("data-theme", "light");
   };
 
+  const toggleTheme = (e) => {
+    if (e.target.checked) {
+      setDarkTheme();
+    } else {
+      setLightTheme();
+    }
+  };
+
   return (
     <div className="dark_mode">
-      <input className="dark_mode_input" type="checkbox" id="darkmode-toggle" />
+      <input
+        className="dark_mode_input"
+        type="checkbox"
+        id="darkmode-toggle"
+        onChange={toggleTheme}
+        defaultChecked={true}
+      />
       <label className="dark_mode_label" htmlFor="darkmode-toggle">
         <Sun />
         <Moon />
