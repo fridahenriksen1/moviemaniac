39b1a931358282dfd7d373f87c220295a5fa4565
Frida Henriksen <fridahenriksen@hotmail.com>
2026-02-07 16:51:39 +0100

    function for dark and light mode

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -3,6 +3,14 @@ import Sun from "../../assets/sun.svg?react";
 import Moon from "../../assets/moon.svg?react";
 
 const DarkMode = () => {
+  const setDarkTheme = () => {
+    document.querySelector("body").setAttribute("data-theme", "dark");
+  };
+
+  const setLightTheme = () => {
+    document.querySelector("body").setAttribute("data-theme", "light");
+  };
+
   return (
     <div className="dark_mode">
       <input className="dark_mode_input" type="checkbox" id="darkmode-toggle" />
