2ae2a00b59e4659e4e3049b178b5bfa570fd8799
Frida Henriksen <fridahenriksen@hotmail.com>
2026-02-24 13:00:38 +0100

    changed tags

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 import "./Navbar.css";
 import DarkMode from "../DarkMode/DarkMode";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>MovieManiac</h1>
       <div className="navbar_links">
-        <a href="#popular">Popular</a>
-        <a href="#top_rated">Top Rated</a>
-        <a href="#upcoming">Upcoming</a>
+        <NavLink to="#popular">Popular</NavLink>
+        <NavLink to="#top_rated">Top Rated</NavLink>
+        <NavLink to="#upcoming">Upcoming</NavLink>
         <DarkMode />
       </div>
     </nav>
