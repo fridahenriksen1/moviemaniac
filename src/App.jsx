54c649a9e317c42e607dd233dc7d385b8ff7fa83
Frida Henriksen <fridahenriksen@hotmail.com>
2026-02-24 13:28:50 +0100

    SingleMovie component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import MovieList from "./components/MovieList/MovieList";
+import SingleMovie from "./components/MovieList/SingleMovie";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
             path="/upcoming"
             element={<MovieList type="upcoming" title="Upcoming" />}
           />
+          <Route path="/movie/:id" element={<SingleMovie />} />
         </Routes>
       </main>
     </div>
